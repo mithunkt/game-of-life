04e69d694d9fae451c8f3ff2207d74f32eb751c0
mithunkt <mithun.kaitherithachareyath@hydro.com.au>
2021-01-19 21:27:16 +1100

    Updates

---
+++ precache-manifest.5fc63d0c24091dd0f35f440a27653e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fce2deb2c9c5c13d3c124ac587eeab",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "d964998ea7c0c2d8b2a1",
+    "url": "/game-of-life/static/css/main.d9b29f4d.chunk.css"
+  },
+  {
+    "revision": "07f7db603bb2148ed761",
+    "url": "/game-of-life/static/js/2.ca9b632a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/game-of-life/static/js/2.ca9b632a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d964998ea7c0c2d8b2a1",
+    "url": "/game-of-life/static/js/main.d2357aad.chunk.js"
+  },
+  {
+    "revision": "cb3a686ed8f9e0cbc460",
+    "url": "/game-of-life/static/js/runtime-main.b1d48015.js"
+  }
+]);
